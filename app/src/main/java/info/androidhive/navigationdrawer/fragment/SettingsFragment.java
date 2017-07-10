337739c50bf7d059c934773e09f29b047e830347
prahastagiri <iputuadiprahastagiri@gmail.com>
2017-07-10 13:58:32 +0700

    Commit
    Application run succesfully
    Fragment title fixed

--- app/src/main/java/info/androidhive/navigationdrawer/fragment/SettingsFragment.java
+++ app/src/main/java/info/androidhive/navigationdrawer/fragment/SettingsFragment.java
@@ -7,33 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+
 
 import info.androidhive.navigationdrawer.R;
 
-import static info.androidhive.navigationdrawer.R.id.listView;
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SettingsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SettingsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    ListView test;
-    String[] NAMES = {"Edit profile","Notification settings","Connected Accounts","Privacy","Account settings"};
-    String[] DESCRIPTIONS ={"Change your name,description and profile photo","choose what alerts and notification you want to see","set facebook,Twitter and Instgram permissions","Hide your profile from search engines","Change your email/password or delete your account"};
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -43,14 +24,6 @@ public class SettingsFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static SettingsFragment newInstance(String param1, String param2) {
         SettingsFragment fragment = new SettingsFragment();
@@ -67,45 +40,6 @@ public class SettingsFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-        test= (ListView) test.findViewById(listView);
-
-        CustomAdapter customAdapter=new CustomAdapter();
-
-        test.setAdapter(customAdapter);
-    }
-
-    class CustomAdapter extends BaseAdapter{
-
-        @Override
-        public int getCount() {
-            return 0;
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            view = getLayoutInflater().inflate(R.layout.customlayoutview,null);
-            TextView textView_Names =(TextView)view.findViewById(R.id.textView3_Names);
-            TextView textView_Descriptions =(TextView)view.findViewById(R.id.textView4_Descriptions);
-
-            textView_Names.setText(NAMES[i]);
-            textView_Descriptions.setText(DESCRIPTIONS[i]);
-            return view;
-        }
-
-        private LayoutInflater getLayoutInflater() {
-            return null;
         }
     }
 
